a2b6698b6c38354d297541bff9a6e4721e5e85d0
DevVux <hoangvu55@gmail.com>
2023-03-30 11:24:04 +0700

    intern2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,5 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-
-const cx = classNames.bind(styles);
-
 function Header() {
-    return (
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
-        </header>
-    );
+    return <h2>Header</h2>;
 }
 
 export default Header;
